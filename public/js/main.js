b65d84752f65dc313013be5fce7b3cd6b6c517c5
Jafar Thwahrah <jaffardawahreh2@gmail.com>
2023-05-14 00:17:46 +0300

    add ffmpeg

--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,51 @@
 const socket = io();
-const chatMessages = document.querySelector('.chat-messages');
-const chatForm = document.getElementById('chat-form')
-const {username , room} = Qs.parse(location.search,{
-    ignoreQueryPrefix:true
-})
+const chatMessages = document.querySelector(".chat-messages");
+const chatForm = document.getElementById("chat-form");
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 //join room
-socket.emit('joinRoom',{username , room})
+socket.emit("joinRoom", { username, room });
 
-socket.on('message', (message)=>{
-    outputMessage(message)
-    chatMessages.scrollTop = chatMessages.scrollHeight;
+socket.on("message", (message) => {
+  outputMessage(message);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
 
-})
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const msg = e.target.elements.msg.value;
 
-chatForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const msg = e.target.elements.msg.value;
-
-    socket.emit('chatMessage',msg)
-    e.target.elements.msg.value=""
-    e.target.elements.msg.focus()
-
-})
+  socket.emit("chatMessage", msg);
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
 
 function outputMessage(message) {
-    const div = document.createElement('div');
-    div.classList.add('message');
-    const p = document.createElement('p');
-    p.classList.add('meta');
-    p.innerText =`${message.username}`;
-    p.innerHTML += `<span> ${message.time}</span>`;
-    div.appendChild(p);
-    const para = document.createElement('p');
-    para.classList.add('text');
-    para.innerText = message.text;
-    div.appendChild(para);
-    document.querySelector('.chat-messages').appendChild(div);
-  }
-  fileForm = document.getElementById('file-form')
-  fileForm.addEventListener('submit' , (e)=>{
-    e.preventDefault();
-    const fileInput = document.querySelector('input[type="file"]');
-    const formData = new FormData();
-    formData.append('uploaded', fileInput.files[0]);
-    fetch("http://localhost:3000/upload-file", {
-        method: 'POST',
-        body: formData
-      })
-        .then(response => console.log(response))
-        .catch(error => console.error(error));
+  const div = document.createElement("div");
+  div.classList.add("message");
+  const p = document.createElement("p");
+  p.classList.add("meta");
+  p.innerText = `${message.username}`;
+  p.innerHTML += `<span> ${message.time}</span>`;
+  div.appendChild(p);
+  const para = document.createElement("p");
+  para.classList.add("text");
+  para.innerText = message.text;
+  div.appendChild(para);
+  document.querySelector(".chat-messages").appendChild(div);
+}
+fileForm = document.getElementById("file-form");
+fileForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const fileInput = document.querySelector('input[type="file"]');
+  const formData = new FormData();
+  formData.append("uploaded", fileInput.files[0]);
+  fetch("http://localhost:3000/upload-file", {
+    method: "POST",
+    body: formData,
   })
+    .then((response) => console.log(response))
+    .catch((error) => console.error(error));
+});
--- server.js
+++ server.js
@@ -1,79 +1,83 @@
-const express = require('express')
-const path = require('path');
-const http = require('http');
-const socketio = require('socket.io');
-const { log } = require('console');
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const { log } = require("console");
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 const io = socketio(server);
 const formatMessage = require("./Models/message");
-const { userJoin,userLeave,getCurrentUser } = require('./Models/user');
-const { spawn } = require('child_process');
-const ffmpegPath = require('ffmpeg-static'); 
+const { userJoin, userLeave, getCurrentUser } = require("./Models/user");
+const { spawn } = require("child_process");
+const ffmpegPath = require("ffmpeg-static");
 //static file
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
+const multer = require("multer");
+const upload = multer({ dest: path.join(__dirname, "uploads") });
 
-const multer = require('multer');
-const upload = multer({ dest:path.join (__dirname , 'uploads') });
-const converted = multer({ dest:path.join (__dirname , 'converted') });
-
-app.post('/upload-file', upload.single('uploaded'),async (req, res) => {
-    console.log(req.file); // the uploaded file
-
-     // Get the path of the uploaded file
+app.post("/upload-file", upload.single("uploaded"), async (req, res) => {
+  // Get the path of the uploaded file
   const filePath = req.file.path;
+
+  // Set output path for the ffmpeg output file
+  const outputPath = path.join(__dirname, "ffmpegOutput");
 
   // Spawn a child process to run ffmpeg
   const ffmpeg = spawn(ffmpegPath, [
-    '-i', filePath,
-    '-vf', 'scale=320:240',
-    'resized.pdf'
+    "-i",
+    filePath,
+    "-vf",
+    "scale=320:240",
+    "-y", // Overwrite output file if it exists
+    "-hide_banner",
+    path.join(outputPath, "resized.mp4"),
   ]);
 
   // Handle output from ffmpeg
-  ffmpeg.stderr.on('data', (data) => {
+  ffmpeg.stderr.on("data", (data) => {
     console.log(`ffmpeg output: ${data}`);
-    res.send(data)
-    res.setHeader('Content-Type', 'text/plain');
-
+    res.write(data);
   });
 
   // Handle completion of ffmpeg
-  ffmpeg.on('close', (code) => {
+  ffmpeg.on("close", (code) => {
     console.log(`ffmpeg exited with code ${code}`);
+    res.end();
   });
-    
-  });
-const botName = 'Bot'
+});
+const botName = "Bot";
 //on connection
-io.on('connection',(socket)=>{
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
 
-    socket.on('joinRoom',({username,room})=>{
+    socket.emit("message", formatMessage(botName, "welcome to chat"));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+  });
 
-        const user = userJoin(socket.id , username , room)
-        socket.join(user.room)
+  socket.on("chatMessage", (message) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", formatMessage(user.username, message));
+  });
 
-
-        socket.emit('message' , formatMessage(botName,"welcome to chat"))
-    socket.broadcast.to(user.room).emit('message', formatMessage(botName,`${user.username} has joined the chat`));
-    })
-
-        socket.on('chatMessage',message =>{
-           const user = getCurrentUser(socket.id)
-        io.to(user.room).emit('message' ,formatMessage(user.username,message) )
-    })
-
-    socket.on('disconnect' , ()=>{
-const user = userLeave(socket.id)
-if(user){
-        io.to(user.room).emit('message',formatMessage(botName,`${user.username} has left the chat`))
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
     }
-    })
-})
-
-
+  });
+});
 
 const PORT = 3000;
-server.listen(PORT, ()=>{
-    console.log(`server running on port ${PORT}`);
-})
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
